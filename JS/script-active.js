36f6d587ffe28047a82299137232c288a687e95f
inert-one <shubhamdubey57@gmail.com>
2021-01-07 20:01:46 +0530

    Initial commit

---
+++ JS/script-active.js
@@ -0,0 +1,48 @@
+var divs = document.querySelectorAll("div");
+
+onscroll = function () {
+  var scrollPosition = document.documentElement.scrollTop;
+
+  divs.forEach((section) => {
+    if (
+      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+      scrollPosition <
+        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+    ) {
+      var currentId = section.attributes.id.value;
+      removeAllActiveClasses();
+      addActiveClass(currentId);
+    }
+  });
+};
+
+var removeAllActiveClasses = function () {
+  document.querySelectorAll("nav ul li a").forEach((el) => {
+    el.classList.remove("active");
+  });
+};
+
+var addActiveClass = function (id) {
+  // console.log(id);
+  var selector = `nav ul li a[href="#${id}"]`;
+  document.querySelector(selector).classList.add("active");
+};
+
+var navLinks = document.querySelectorAll("nav ul li a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    var currentId = e.target.attributes.href.value;
+    var section = document.querySelector(currentId);
+    var sectionPos = section.offsetTop;
+    // section.scrollIntoView({
+    //   behavior: "smooth",
+    // });
+
+    window.scroll({
+      top: sectionPos - 40,
+      behavior: "smooth",
+    });
+  });
+});
---
+++ JS/script.js
@@ -0,0 +1,148 @@
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+        if($(window).scrollTop()) {
+              $('nav').addClass('black');
+        }
+
+        else {
+              $('nav').removeClass('black');
+        }
+  })
+
+    $('li').on("click", function(){
+        if($('li').hasClass('active')){
+            $('li').removeClass('active');
+            $(this).addClass('active');
+        }
+    })
+
+
+
+//     Auto Scrolling
+
+function pageScroll() {
+      window.scrollBy(0,1);
+      scrolldelay = setTimeout(pageScroll,15);
+  }
+function scrollOff(){
+            clearTimeout(scrolldelay);
+      }
+
+window.addEventListener("touchmove", scrollOff )
+window.addEventListener("mousemove", scrollOff )
+
+// Slideshow
+
+// var myIndex = 0;
+// carousel();
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("Project-slides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {myIndex = 1}    
+//   x[myIndex-1].style.display = "block";  
+//   setTimeout(carousel, 4000); // Change image every 2 seconds
+// }
+
+
+//Reveal animation
+new WOW().init();
+
+// Slick
+
+
+
+      if (window.matchMedia('(min-width: 767px)').matches) {
+          //...
+          $(document).ready(function(){
+            $('.slides').slick({
+                  dots: true,
+                  infinite: true,
+                  speed: 500,
+                  fade: true,
+                  autoplay:true,
+                  autoplaySpeed: 4000,
+                  cssEase: 'linear'
+      });
+});   
+      }else{
+          $(document).ready(function(){
+          $('.slides').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+                  });
+                });
+            
+      }
+
+
+//   Responsive nav
+
+// function myFunction() {
+//       var x = document.getElementById("button-container");
+//       if (x.className === "topnav") {
+//         x.className += " responsive";
+//       } else {
+//         x.className = "topnav";
+//       }
+//     }
+
+
+    // Hamburger
+
+
+function myFunction(){
+      var main = document.getElementById("mySidebar");
+      if (main.className ==="sidebar"){
+      main.className += " sideOn";
+}
+else{
+      main.className = "sidebar";
+}
+}
+
+
+$('.Contacts').mousemove(function(e){
+      var amountMovedX = (e.pageX * 1 / 20);
+      var amountMovedY = (e.pageY * 1 / 20);
+      $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+  });
+// // Smooth scroll
+
+
+
+
+
+
+
+
+
+// function scrollToHash(hash1) {
+//       location.hash = "#" + hash1;
+//   }
+
+// Active on Scroll 
+
+// div = document.querySelectorAll('div');
+// li = document.querySelectorAll('li');
+
+// onscroll = function(){
+//       var scrollPosition = document.documentElement.scrollTop;
+
+//       div.forEach( div => {
+//             if(scrollPosition >= div.offsetTop && scrollPosition < div.offsetTop + div.offsetHeight){
+//                   let currentId = div.attributes.id.value;      
+//                   if (li.hasClass('active')){
+//                             li.removeClass('active');
+//                             currentId.addClass('active');}
+//             }
+//       })
+// }
+
