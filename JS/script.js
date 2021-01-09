d99e395f789ba7e7245ec574f3302fb1924da490
inert-one <shubhamdubey57@gmail.com>
2021-01-09 07:03:37 +0530

    Update 5

--- JS/script.js
+++ JS/script.js
@@ -23,7 +23,7 @@
 
 function pageScroll() {
       window.scrollBy(0,1);
-      scrolldelay = setTimeout(pageScroll,15);
+      var scrolldelay = setTimeout(pageScroll,10);
   }
 function scrollOff(){
             clearTimeout(scrolldelay);
@@ -83,20 +83,6 @@ new WOW().init();
       }
 
 
-//   Responsive nav
-
-// function myFunction() {
-//       var x = document.getElementById("button-container");
-//       if (x.className === "topnav") {
-//         x.className += " responsive";
-//       } else {
-//         x.className = "topnav";
-//       }
-//     }
-
-
-    // Hamburger
-
 
 function myFunction(){
       var overlay = document.getElementById("body-overlay");
@@ -117,35 +103,87 @@ $('.Contacts').mousemove(function(e){
       var amountMovedY = (e.pageY * 1 / 20);
       $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
   });
-// // Smooth scroll
+
+// ES6 Class
+class TypeWriter {
+      constructor(txtElement, words, wait = 3000) {
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = '';
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 10);
+        this.type();
+        this.isDeleting = false;
+      }
+    
+      type() {
+        // Current index of word
+        const current = this.wordIndex % this.words.length;
+        // Get full text of current word
+        const fullTxt = this.words[current];
+    
+        // Check if deleting
+        if(this.isDeleting) {
+          // Remove char
+          this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+          // Add char
+          this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+    
+        // Insert txt into element
+        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+    
+        // Initial Type Speed
+        let typeSpeed = 80;
+    
+        if(this.isDeleting) {
+          typeSpeed /= 2;
+        }
+    
+        // If word is complete
+        if(!this.isDeleting && this.txt === fullTxt) {
+          // Make pause at end
+          typeSpeed = this.wait;
+          // Set delete to true
+          this.isDeleting = true;
+        } else if(this.isDeleting && this.txt === '') {
+          this.isDeleting = false;
+          // Move to next word
+          this.wordIndex++;
+          // Pause before start typing
+          typeSpeed = 0;
+        }
+    
+        setTimeout(() => this.type(), typeSpeed);
+      }
+    }
+    
+    
+    // Init On DOM Load
+    document.addEventListener('DOMContentLoaded', init);
+    
+    // Init App
+    function init() {
+      const txtElement = document.querySelector('.txt-type');
+      const words = JSON.parse(txtElement.getAttribute('data-words'));
+      const wait = txtElement.getAttribute('data-wait');
+      // Init TypeWriter
+      new TypeWriter(txtElement, words, wait);
+    }
 
 
+// TypeWriter 2
 
+var i = 0;
+var txt = "Hi, I'm Shubham."; /* The text */
+var speed = 300; /* The speed/duration of the effect in milliseconds */
 
-
-
-
-
-
-// function scrollToHash(hash1) {
-//       location.hash = "#" + hash1;
-//   }
-
-// Active on Scroll 
-
-// div = document.querySelectorAll('div');
-// li = document.querySelectorAll('li');
-
-// onscroll = function(){
-//       var scrollPosition = document.documentElement.scrollTop;
-
-//       div.forEach( div => {
-//             if(scrollPosition >= div.offsetTop && scrollPosition < div.offsetTop + div.offsetHeight){
-//                   let currentId = div.attributes.id.value;      
-//                   if (li.hasClass('active')){
-//                             li.removeClass('active');
-//                             currentId.addClass('active');}
-//             }
-//       })
-// }
-
+function typeWriter2() {
+  if (i < txt.length) {
+    document.getElementById("type-stop").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter2, speed);
+  }
+}
+typeWriter2();
